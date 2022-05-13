460f89fc6ad1023e6229d0f3b34f12d42b610577
Sheifer Kepler <sheifer.kda@gmail.com>
2022-05-12 21:36:10 -0300

    iniciado exercícios 4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script-2.js
@@ -0,0 +1,6 @@
+const a = 175;
+const b = 120;
+
+let subtracao = a - b;
+
+console.log(subtracao);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script-7.js
@@ -0,0 +1,16 @@
+const a = 50;
+const b = 50;
+const c = 50;
+
+if(a > b && a > c){
+    console.log(a)
+}
+else if (b > a && b > c){
+    console.log(b)
+}
+else if(c > a && c > b){
+    console.log(c)
+}
+else{
+    console.log("números iguais")
+}
