cadb62eaec1d9fef255d6fc68f3fa9c1b6339432
Sheifer Kepler <sheifer.kda@gmail.com>
2022-07-06 19:24:54 -0300

    exercícios dia 9.1

---
+++ front-end/dia-9.1-JavaScript-e-Testes-Assíncronos/script - 4.js
@@ -0,0 +1,11 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = (callback) => console.log(`Mars temperature is: ${callback()} degree Celsius`);
+
+setTimeout(() => sendMarsTemperature(getMarsTemperature), 5000); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
