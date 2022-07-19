9b9d3c6d83a1cc7e7947d0fbd3280fcfb1837dce
Sheifer Kepler <sheifer.kda@gmail.com>
2022-07-19 11:55:41 -0300

    exercício 3 css

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-3/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-3/src/App.js
@@ -8,7 +8,7 @@ import Footer from './Footer';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className='all'>
         <header>
           <Header />
         </header>
